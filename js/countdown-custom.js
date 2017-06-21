1d66dd37f532b6b0b7537d14e3373c1a39edf872
Jashan Preet Singh <Jashan Preet Singh>
2017-06-21 21:35:11 +0530

    Commit JP

---
+++ js/countdown-custom.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function() {
+        jQuery(function () {
+            jQuery('#defaultCountdown').countdown({until: new Date(2017, 6-1, 25, 11)}); // year, month, date, hour
+        });
+});		
+
