fcaadd17b19ac776739ef3d42e0547b36c603029
Jashan Preet Singh <Jashan Preet Singh>
2017-07-03 16:29:24 +0530

    next event

--- js/countdown-custom.js
+++ js/countdown-custom.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function() {
         jQuery(function () {
-            jQuery('#defaultCountdown').countdown({until: new Date(2017, 6-1, 28, 11)}); // year, month, date, hour
+            jQuery('#defaultCountdown').countdown({until: new Date(2017, 7-1, 8, 11)}); // year, month, date, hour
         });
 });		
 
